27f505fe0e72812766ba62c76f1db600a0c13225
Sami <sami.kalmi@gmail.com>
2014-02-28 05:41:08 +0200

    Added a unity project with a sample scene.

---
+++ Narcissos/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+	public float movementSpeed = 10.0f;
+
+	public static bool isMoving;
+
+	private Vector2 targetPosition;
+	private Vector2 movementDirection;
+	private Vector2 moveStartPos;
+	private float moveLength;
+
+	void Awake () {
+		targetPosition = transform.position;
+		isMoving = false;
+	}
+
+	void Update () {
+		if (Input.GetMouseButtonUp (0) && ((CursorControl.lookable == false) || (CursorControl.useHand == true))) {
+			SetMoveTarget();
+		}
+		if   (((Vector2)transform.position - (Vector2)moveStartPos).sqrMagnitude < moveLength) {
+			if (transform.FindChild("PlaceHolderChar").animation.isPlaying != true) {
+				transform.FindChild("PlaceHolderChar").animation.Play();
+			}
+			transform.Translate(movementDirection * movementSpeed * Time.deltaTime);
+			isMoving = true;
+		}
+		else {
+			transform.FindChild("PlaceHolderChar").animation.Stop();
+			isMoving = false;
+		}
+	}
+
+	void SetMoveTarget () {
+		targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		moveStartPos = transform.position;
+		moveLength = ((Vector2)targetPosition - (Vector2)transform.position).sqrMagnitude;
+		movementDirection = ( (Vector2) targetPosition -  (Vector2) transform.position).normalized;
+		Debug.Log ("Clicked, TargetPosition = " + targetPosition + " and movementDirection = " + movementDirection);
+	}
+}
---
+++ Narcissos/Assets/Scripts/ItemBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemBehaviour : MonoBehaviour {
+
+	public GameObject InventoryVersion;
+	private bool isTarget;
+
+	void OnMouseOver () {
+		CursorControl.lookable = true;
+	}
+
+	void OnMouseExit () {
+		CursorControl.lookable = false;
+	}
+
+	void Update () {
+		if (Input.GetMouseButtonUp(0)) {
+			Debug.Log ((Camera.main.ScreenToWorldPoint(Input.mousePosition)-transform.position).sqrMagnitude);
+		    if ((Camera.main.ScreenToWorldPoint(Input.mousePosition)-transform.position).sqrMagnitude < 101) {
+				if (CursorControl.useHand == true) {
+					isTarget = true;
+					StartCoroutine (BePickedUp());
+				}
+			}
+			else {isTarget = false;}
+		}
+	}
+
+	IEnumerator BePickedUp() {
+		Debug.Log ("Start Picking up");
+		yield return new WaitForSeconds (0.1f);
+		while (CharacterMovement.isMoving == true) {
+			yield return null;
+		}
+		Debug.Log ("Stopped Moving in PickUp");
+		yield return new WaitForSeconds (0.1f);
+		if (isTarget == true) {
+			Debug.Log ("Object Destroyed");
+			CursorControl.lookable = false;
+			InventoryVersion.SetActive(true);
+			GameObject.FindGameObjectWithTag("Inventory").SendMessage("ShowInventory");
+			Destroy(gameObject);
+		}
+	}
+}
